558f5d29fa2fc5efd99214eb26aee9b3f448ac6a
Stanislav Stativa <stas.stativa@gmail.com>
2025-03-28 12:10:37 +0200

    Revert "fix bugs"
    This reverts commit d2319036c8e75bd4d74817ec3752f694354791bf.

--- src/sxastarter/src/lib/page-props-factory/plugins/preview-mode.ts
+++ src/sxastarter/src/lib/page-props-factory/plugins/preview-mode.ts
@@ -6,7 +6,7 @@ import {
 } from '@sitecore-jss/sitecore-jss-nextjs';
 import {
   editingDataService,
-  isDesignLibraryPreviewData,
+  isComponentLibraryPreviewData,
   isEditingMetadataPreviewData,
 } from '@sitecore-jss/sitecore-jss-nextjs/editing';
 import { SitecorePageProps } from 'lib/page-props';
@@ -21,7 +21,7 @@ class PreviewModePlugin implements Plugin {
   async exec(props: SitecorePageProps, context: GetServerSidePropsContext | GetStaticPropsContext) {
     if (!context.preview) return props;
 
-    if (isDesignLibraryPreviewData(context.previewData)) {
+    if (isComponentLibraryPreviewData(context.previewData)) {
       const { itemId, componentUid, site, language, renderingId, dataSourceId, version } =
         context.previewData;
 
