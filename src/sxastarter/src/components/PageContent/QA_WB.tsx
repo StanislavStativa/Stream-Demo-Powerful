87fe0cee3296c8f4aad54e51f9763a357d990c1e
Wei Bin Chiu <weibin.chiu@sitecore.com>
2025-04-04 14:58:54 +0800

    add QA_WB component

---
+++ src/sxastarter/src/components/PageContent/QA_WB.tsx
@@ -0,0 +1,54 @@
+import { Field, Image, ImageField, Link } from '@sitecore-jss/sitecore-jss-nextjs';
+
+interface Fields {
+  Title: Field<string>;
+  File: Field<unknown>;
+  Image: ImageField;
+}
+
+export type DemoComponentProps = {
+  params: { [key: string]: string };
+  fields: Fields;
+};
+
+const ComponentDefault = (props: DemoComponentProps): JSX.Element => (
+  <div className={`component ${props.params?.styles}`.trimEnd()}>
+    <div className="component-content">
+      <span className="is-empty-hint">QA WB Default</span>
+    </div>
+  </div>
+);
+
+export const Default = (props: DemoComponentProps): JSX.Element => {
+  const id = props.params.RenderingIdentifier;
+
+  console.log('DemoComponent', props.fields);
+
+  return props.fields ? (
+    <div className={`component QA_WB ${props.params?.styles?.trimEnd()}`} id={id ? id : undefined}>
+      <div className="container">
+        <div className="pt-2">
+          <strong>Image</strong>
+          <div>
+            <Image field={props.fields.Image} />
+          </div>
+        </div>
+        <div className="pt-2">
+          <strong>FileField</strong> -{' '}
+          <Link
+            field={{
+              value: {
+                href: (props.fields.File.value as { src: string }).src,
+                text: `${(props.fields.File.value as { displayName: string }).displayName}.${(
+                  props.fields.File.value as { extension: string }
+                ).extension.toLowerCase()}`,
+              },
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  ) : (
+    <ComponentDefault {...props} />
+  );
+};
