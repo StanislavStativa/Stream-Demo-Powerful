f95c2df6435f56759345dda5457209d9924e1152
Stanislav Stativa <stas.stativa@gmail.com>
2025-09-04 21:54:59 +0300

    Trigger a new Code extraction 2

--- src/sxastarter/src/components/RichText.tsx
+++ src/sxastarter/src/components/RichText.tsx
@@ -1,6 +1,6 @@
 // SDK branch
 // Added during our Demo meeting
-// Trigger a new Code extraction
+// Trigger a new Code extraction #2
 import { Field, RichText } from '@sitecore-content-sdk/nextjs';
 import React, { JSX } from 'react';
 
