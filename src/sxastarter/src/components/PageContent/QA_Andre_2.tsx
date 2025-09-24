bc363b22706373008ae348a9b59bc62850095855
Andre Ong <ong@sitecore.net>
2025-09-24 14:54:22 +0800

    update qa andre 2 ai variant

--- src/sxastarter/src/components/PageContent/QA_Andre_2.tsx
+++ src/sxastarter/src/components/PageContent/QA_Andre_2.tsx
@@ -4,7 +4,7 @@ import {
   RichText,
   RichTextField,
   Text,
-  Link, // Importing Link for General Link handling
+  Link,
   LinkField,
 } from '@sitecore-content-sdk/nextjs';
 
@@ -59,6 +59,7 @@ const ComponentDefault = (props: QAProps): JSX.Element => (
     </div>
   </div>
 );
+
 export const Default = (props: QAProps): JSX.Element => {
   const id = props.params.RenderingIdentifier;
   return props.fields ? (
@@ -313,235 +314,174 @@ export const Default = (props: QAProps): JSX.Element => {
 
 export const PrettierByAI = (props: QAProps): JSX.Element => {
   const id = props.params.RenderingIdentifier;
+  const fieldBlock = (title: string, content: JSX.Element) => (
+    <div className="tile bg-gray-100 rounded-md shadow-md p-4 mb-4">
+      <h3 className="tile-title text-lg font-bold mb-2">{title}</h3>
+      <div>{content}</div>
+    </div>
+  );
+
   return props.fields ? (
     <div
       className={`component variant-grid ${props?.params?.styles?.trimEnd()}`}
       id={id ? id : undefined}
     >
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        {/* Single Line Text */}
-        <div className="tile bg-gray-100 rounded-md shadow-md p-4">
-          <h3 className="tile-title text-lg font-bold mb-2">Single Line Text</h3>
-          <div className="whitespace-pre-line">
+        {fieldBlock(
+          'Single Line Text',
+          <>
             <Text field={props.fields?.SingleLine1} />
             <br />
             <Text field={props.fields?.SingleLine2} />
-          </div>
-        </div>
-
-        {/* Multi Line Text */}
-        <div className="tile bg-gray-100 rounded-md shadow-md p-4">
-          <h3 className="tile-title text-lg font-bold mb-2">Multi Line Text</h3>
-          <div className="whitespace-pre-line">
+          </>
+        )}
+        {fieldBlock(
+          'Multi Line Text',
+          <>
             <Text field={props.fields?.MultiLine1} />
             <br />
             <Text field={props.fields?.MultiLine2} />
-          </div>
-        </div>
-
-        {/* Rich Text */}
-        <div className="tile bg-gray-100 rounded-md shadow-md p-4">
-          <h3 className="tile-title text-lg font-bold mb-2">Rich Text</h3>
-          <div>
+          </>
+        )}
+        {fieldBlock(
+          'Rich Text',
+          <>
             <RichText field={props.fields?.RichText1} />
-          </div>
-          <div>
             <RichText field={props.fields?.RichText2} />
-          </div>
-        </div>
-
-        {/* Integer */}
-        <div className="tile bg-gray-100 rounded-md shadow-md p-4">
-          <h3 className="tile-title text-lg font-bold mb-2">Integer</h3>
-          <div className="whitespace-pre-line">
+          </>
+        )}
+        {fieldBlock(
+          'Integer',
+          <>
             <Text field={props.fields?.Integer1} />
             <br />
             <Text field={props.fields?.Integer2} />
-          </div>
-        </div>
-
-        {/* Number */}
-        <div className="tile bg-gray-100 rounded-md shadow-md p-4">
-          <h3 className="tile-title text-lg font-bold mb-2">Number</h3>
-          <div className="whitespace-pre-line">
+          </>
+        )}
+        {fieldBlock(
+          'Number',
+          <>
             <Text field={props.fields?.Number1} />
             <br />
             <Text field={props.fields?.Number2} />
-          </div>
-        </div>
-
-        {/* Date */}
-        <div className="tile bg-gray-100 rounded-md shadow-md p-4">
-          <h3 className="tile-title text-lg font-bold mb-2">Date</h3>
-          <div className="whitespace-pre-line">
+          </>
+        )}
+        {fieldBlock(
+          'Date',
+          <>
             <Text field={props.fields?.Date1} />
             <br />
             <Text field={props.fields?.Date2} />
-          </div>
-        </div>
-
-        {/* Datetime */}
-        <div className="tile bg-gray-100 rounded-md shadow-md p-4">
-          <h3 className="tile-title text-lg font-bold mb-2">Datetime</h3>
-          <div className="whitespace-pre-line">
+          </>
+        )}
+        {fieldBlock(
+          'Datetime',
+          <>
             <Text field={props.fields?.DateTime1} />
             <br />
             <Text field={props.fields?.DateTime2} />
-          </div>
-        </div>
-
-        {/* Multilist */}
-        <div className="tile bg-gray-100 rounded-md shadow-md p-4">
-          <h3 className="tile-title text-lg font-bold mb-2">Multilist</h3>
-          <div className="text-sm whitespace-pre-line">
-            {Array.isArray(props.fields?.MultiList1?.value) &&
-              props.fields?.MultiList1?.value
-                .map((item: { label: string }) => item?.label)
-                .join('\n')}
-          </div>
-          <div className="text-sm whitespace-pre-line">
-            {Array.isArray(props.fields?.MultiList2?.value) &&
-              props.fields?.MultiList2?.value
-                .map((item: { label: string }) => item?.label)
-                .join('\n')}
-          </div>
-        </div>
-
-        {/* Multilist with Search */}
-        <div className="tile bg-gray-100 rounded-md shadow-md p-4">
-          <h3 className="tile-title text-lg font-bold mb-2">Multilist with Search</h3>
-          <div className="text-sm whitespace-pre-line">
-            {Array.isArray(props.fields?.MultiListSearch1?.value) &&
-              props.fields?.MultiListSearch1?.value
-                .map((item: { label: string }) => item?.label)
-                .join('\n')}
-          </div>
-          <div className="text-sm whitespace-pre-line">
-            {Array.isArray(props.fields?.MultiListSearch2?.value) &&
-              props.fields?.MultiListSearch2?.value
-                .map((item: { label: string }) => item?.label)
-                .join('\n')}
-          </div>
-        </div>
-
-        {/* Tree List */}
-        <div className="tile bg-gray-100 rounded-md shadow-md p-4">
-          <h3 className="tile-title text-lg font-bold mb-2">Tree List</h3>
-          <div className="text-sm whitespace-pre-line">
-            {Array.isArray(props.fields?.TreeList1?.value) &&
-              props.fields?.TreeList1?.value
-                .map((item: { label: string }) => item?.label)
-                .join('\n')}
-          </div>
-          <div className="text-sm whitespace-pre-line">
-            {Array.isArray(props.fields?.TreeList2?.value) &&
-              props.fields?.TreeList2?.value
-                .map((item: { label: string }) => item?.label)
-                .join('\n')}
-          </div>
-        </div>
-
-        {/* Multiroot Tree List */}
-        <div className="tile bg-gray-100 rounded-md shadow-md p-4">
-          <h3 className="tile-title text-lg font-bold mb-2">Multiroot Tree List</h3>
-          <div className="text-sm whitespace-pre-line">
-            {Array.isArray(props.fields?.MultiRootTreeList1?.value) &&
-              props.fields?.MultiRootTreeList1?.value
-                .map((item: { label: string }) => item?.label)
-                .join('\n')}
-          </div>
-          <div className="text-sm whitespace-pre-line">
-            {Array.isArray(props.fields?.MultiRootTreeList2?.value) &&
-              props.fields?.MultiRootTreeList2?.value
-                .map((item: { label: string }) => item?.label)
-                .join('\n')}
-          </div>
-        </div>
-
-        {/* Checklist */}
-        <div className="tile bg-gray-100 rounded-md shadow-md p-4">
-          <h3 className="tile-title text-lg font-bold mb-2">Checklist</h3>
-          <div className="text-sm whitespace-pre-line">
-            {Array.isArray(props.fields?.CheckList1?.value) &&
-              props.fields?.CheckList1?.value
-                .map((item: { label: string }) => item?.label)
-                .join('\n')}
-          </div>
-          <div className="text-sm whitespace-pre-line">
-            {Array.isArray(props.fields?.CheckList2?.value) &&
-              props.fields?.CheckList2?.value
-                .map((item: { label: string }) => item?.label)
-                .join('\n')}
-          </div>
-        </div>
-
-        {/* Taglist */}
-        <div className="tile bg-gray-100 rounded-md shadow-md p-4">
-          <h3 className="tile-title text-lg font-bold mb-2">Tag List</h3>
-          <div className="text-sm whitespace-pre-line">
-            {Array.isArray(props.fields?.TagList1?.value) &&
-              props.fields?.TagList1?.value
-                .map((item: { label: string }) => item?.label)
-                .join('\n')}
-          </div>
-          <div className="text-sm whitespace-pre-line">
-            {Array.isArray(props.fields?.TagList2?.value) &&
-              props.fields?.TagList2?.value
-                .map((item: { label: string }) => item?.label)
-                .join('\n')}
-          </div>
-        </div>
-
-        {/* Checkbox */}
-        <div className="tile bg-gray-100 rounded-md shadow-md p-4">
-          <h3 className="tile-title text-lg font-bold mb-2">Checkbox</h3>
-          <div className="whitespace-pre-line">
-            <Text field={props.fields?.CheckBox1} />
+          </>
+        )}
+        {fieldBlock(
+          'Multilist',
+          <>
+            {Array.isArray(props.fields.MultiList1.value) &&
+              props.fields.MultiList1.value.map((item: { label: string }) => item.label).join(', ')}
             <br />
-            <Text field={props.fields?.CheckBox2} />
-          </div>
-        </div>
-
-        {/* General Link */}
-        <div className="tile bg-gray-100 rounded-md shadow-md p-4">
-          <h3 className="tile-title text-lg font-bold mb-2">General Link</h3>
-          <div>
-            <Link field={props.fields?.Link1}>
-              {' '}
-              {/* Use Link component from Sitecore SDK */}
-              {props.fields?.Link1?.value?.text || 'Link 1'}
-            </Link>
-          </div>
-          <div>
-            <Link field={props.fields?.Link2}>
-              {' '}
-              {/* Use Link component from Sitecore SDK */}
-              {props.fields?.Link2?.value?.text || 'Link 2'}
-            </Link>
-          </div>
-        </div>
-
-        {/* Droptree */}
-        <div className="tile bg-gray-100 rounded-md shadow-md p-4">
-          <h3 className="tile-title text-lg font-bold mb-2">Droptree</h3>
-          <div>
-            <span>Tree 1: {props.fields?.DropTree1?.value?.label}</span>
-          </div>
-          <div>
-            <span>Tree 2: {props.fields?.DropTree2?.value?.label}</span>
-          </div>
-        </div>
-
-        {/* Droplink */}
-        <div className="tile bg-gray-100 rounded-md shadow-md p-4">
-          <h3 className="tile-title text-lg font-bold mb-2">Droplink</h3>
-          <div>
-            <span>Link 1: {props.fields?.DropLink1?.value?.label}</span>
-          </div>
-          <div>
-            <span>Link 2: {props.fields?.DropLink2?.value?.label}</span>
-          </div>
-        </div>
+            {Array.isArray(props.fields.MultiList2.value) &&
+              props.fields.MultiList2.value.map((item: { label: string }) => item.label).join(', ')}
+          </>
+        )}
+        {fieldBlock(
+          'Multilist with Search',
+          <>
+            {Array.isArray(props.fields.MultiListSearch1.value) &&
+              props.fields.MultiListSearch1.value
+                .map((item: { label: string }) => item.label)
+                .join(', ')}
+            <br />
+            {Array.isArray(props.fields.MultiListSearch2.value) &&
+              props.fields.MultiListSearch2.value
+                .map((item: { label: string }) => item.label)
+                .join(', ')}
+          </>
+        )}
+        {fieldBlock(
+          'Tree List',
+          <>
+            {Array.isArray(props.fields.TreeList1.value) &&
+              props.fields.TreeList1.value.map((item: { label: string }) => item.label).join(', ')}
+            <br />
+            {Array.isArray(props.fields.TreeList2.value) &&
+              props.fields.TreeList2.value.map((item: { label: string }) => item.label).join(', ')}
+          </>
+        )}
+        {fieldBlock(
+          'Multiroot Tree List',
+          <>
+            {Array.isArray(props.fields.MultiRootTreeList1.value) &&
+              props.fields.MultiRootTreeList1.value
+                .map((item: { label: string }) => item.label)
+                .join(', ')}
+            <br />
+            {Array.isArray(props.fields.MultiRootTreeList2.value) &&
+              props.fields.MultiRootTreeList2.value
+                .map((item: { label: string }) => item.label)
+                .join(', ')}
+          </>
+        )}
+        {fieldBlock(
+          'Checklist',
+          <>
+            {Array.isArray(props.fields.CheckList1.value) &&
+              props.fields.CheckList1.value.map((item: { label: string }) => item.label).join(', ')}
+            <br />
+            {Array.isArray(props.fields.CheckList2.value) &&
+              props.fields.CheckList2.value.map((item: { label: string }) => item.label).join(', ')}
+          </>
+        )}
+        {fieldBlock(
+          'Tag List',
+          <>
+            {Array.isArray(props.fields.TagList1.value) &&
+              props.fields.TagList1.value.map((item: { label: string }) => item.label).join(', ')}
+            <br />
+            {Array.isArray(props.fields.TagList2.value) &&
+              props.fields.TagList2.value.map((item: { label: string }) => item.label).join(', ')}
+          </>
+        )}
+        {fieldBlock(
+          'Checkbox',
+          <>
+            <Text field={props.fields.CheckBox1} />
+            <br />
+            <Text field={props.fields.CheckBox2} />
+          </>
+        )}
+        {fieldBlock(
+          'General Link',
+          <>
+            <Link field={props.fields.Link1}>{props.fields.Link1.value?.text || 'Link 1'}</Link>
+            <br />
+            <Link field={props.fields.Link2}>{props.fields.Link2.value?.text || 'Link 2'}</Link>
+          </>
+        )}
+        {fieldBlock(
+          'Droptree',
+          <>
+            <span>Tree 1: {props.fields.DropTree1?.value?.label}</span>
+            <br />
+            <span>Tree 2: {props.fields.DropTree2?.value?.label}</span>
+          </>
+        )}
+        {fieldBlock(
+          'Droplink',
+          <>
+            <span>Link 1: {props.fields.DropLink1?.value?.label}</span>
+            <br />
+            <span>Link 2: {props.fields.DropLink2?.value?.label}</span>
+          </>
+        )}
       </div>
     </div>
   ) : (
